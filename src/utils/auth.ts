a174902ebddd7d1d64b1c0ac0c61a5eda8baffe1
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2020-01-29 07:03:36 +0900

    Googleログインの実装

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,4 +1,5 @@
 import * as Facebook from 'expo-facebook';
+import * as Google from 'expo-google-app-auth';
 import firebase, { FacebookAuthProvider } from '../configs/firebase';
 import facebook from '../configs/facebook';
 
@@ -55,6 +56,69 @@ export const facebookSignin = async (): Promise<UserCred | null> => {
     }
   }
   return null;
+};
+
+// Googelでのユーザ登録
+const isUserEqual = (
+  googleUser: Google.LogInResult,
+  firebaseUser: firebase.User
+): boolean => {
+  if (firebaseUser) {
+    const { providerData } = firebaseUser;
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < providerData.length; i++) {
+      if (
+        providerData[i].providerId ===
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+        providerData[i].uid === googleUser.getBasicProfile().getId()
+      ) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+const onSignInGoolge = (googleUser: Google.LogInResult): void => {
+  const unsubscribe = firebase.auth().onAuthStateChanged(firebaseUser => {
+    unsubscribe();
+    if (!isUserEqual(googleUser, firebaseUser)) {
+      const credential = firebase.auth.GoogleAuthProvider.credential(
+        googleUser.idToken,
+        googleUser.accessToken
+      );
+      firebase
+        .auth()
+        .signInWithCredential(credential)
+        .then(() => {
+          console.log('success');
+        })
+        .catch(({ massage }) => alert('messgage', massage));
+    } else {
+      console.log('User already signed-in Firebase.');
+    }
+  });
+};
+
+export const goolgeSignUp = async () => {
+  try {
+    const result = await Google.logInAsync({
+      behavior: 'web',
+      androidClientId:
+        '975945165729-l58crudfj4bqs737hi2a8m9use1asjrg.apps.googleusercontent.com',
+      iosClientId:
+        '975945165729-dlspp12h2bbol4ecvdm6g06gte1k0adr.apps.googleusercontent.com',
+      scopes: ['profile', 'email'],
+    });
+
+    if (result.type === 'success') {
+      onSignInGoolge(result);
+      return result.accessToken;
+    }
+    return { cancelled: true };
+  } catch (e) {
+    return { error: true };
+  }
 };
 
 export default firebase;
