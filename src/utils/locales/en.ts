1992e66ae884192be0a942ff73ac716d7f39f9bd
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2020-02-12 22:54:19 +0900

    言語ごとの切り替えと開発環境の切り替え

---
+++ src/utils/locales/en.ts
@@ -0,0 +1,11 @@
+const initializeScreen = {
+  initializeStart: 'Start',
+  initializeHaveAcconut: 'do you have',
+  initializeHere: 'here',
+};
+
+const en = {
+  ...initializeScreen,
+};
+
+export default en;
