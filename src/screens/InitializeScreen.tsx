b97d8c01d3e55424c499563f50264986366eb14a
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2020-02-12 22:56:40 +0900

    InitializeScreenの実装完了

--- src/screens/InitializeScreen.tsx
+++ src/screens/InitializeScreen.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
 import { NavigationStackProp } from 'react-navigation-stack';
+import SubmitButton from '../components/atoms/SubmitButton';
+import { fontSizeM, linkBlue, primaryColor } from '../styles/Common';
+import I18n from '../utils/I18n';
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
+  contaner: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingHorizontal: 16,
+    paddingBottom: 32,
+  },
+  text: {
+    color: primaryColor,
+    fontSize: fontSizeM,
+    paddingTop: 16,
+  },
+  linkText: {
+    color: linkBlue,
+  },
+});
 
 /**
  * 概要：ログインしていないユーザの立ち上げ画面
@@ -8,18 +32,26 @@ import { NavigationStackProp } from 'react-navigation-stack';
 const InitializeScreen: React.FC<{ navigation: NavigationStackProp }> = ({
   navigation,
 }): JSX.Element => {
-  const onPressSignUp = (): void => {
-    navigation.navigate('SignUp');
-  };
   const onPressSignIn = (): void => {
     navigation.navigate('SignIn');
   };
 
+  const onPressSignUp = (): void => {
+    navigation.navigate('SignUp');
+  };
+
   return (
-    <View>
-      <Button title="SignUp" onPress={onPressSignUp} />
-      <Button title="SignIn" onPress={onPressSignIn} />
-    </View>
+    <SafeAreaView style={styles.safeAreaView}>
+      <View style={styles.contaner}>
+        <SubmitButton title="はじめる" onPress={onPressSignUp} />
+        <Text style={styles.text}>
+          アカウントをお持ちの方は
+          <Text style={styles.linkText} onPress={onPressSignIn}>
+            こちら
+          </Text>
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
