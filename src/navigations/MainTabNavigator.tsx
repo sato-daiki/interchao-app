be422232cd3e7fdbd81e2bc064e8c2a0b7272a5c
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2020-02-16 19:53:52 +0900

    タブの設定

--- src/navigations/MainTabNavigator.tsx
+++ src/navigations/MainTabNavigator.tsx
@@ -1,32 +1,69 @@
+import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 /* screens */
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import MyDiaryListScreen from '../screens/MyDiaryListScreen';
 import YourDiaryListScreen from '../screens/YourDiaryListScreen';
+import { mainColor } from '../styles/Common';
 
 /* components */
 // import TabIcon from '../components/molecules/TabIcon';
 
-const MainTab = createBottomTabNavigator(
+const MyDiaryTabStack = createStackNavigator(
   {
     MyDiaryList: {
       screen: MyDiaryListScreen,
+    },
+  },
+  {
+    initialRouteName: 'MyDiaryList',
+    headerLayoutPreset: 'center',
+  }
+);
+
+const MainTab = createBottomTabNavigator(
+  {
+    MyDiary: {
+      screen: MyDiaryTabStack,
       navigationOptions: {
         tabBarLabel: 'マイ日記',
+        tabBarIcon: ({ tintColor }: { tintColor: string }): JSX.Element => (
+          <MaterialCommunityIcons
+            name="book-open"
+            size={25}
+            color={tintColor}
+          />
+        ),
       },
     },
-    YourDiaryList: {
+    WriteDiary: {
+      screen: YourDiaryListScreen,
+      navigationOptions: {
+        tabBarLabel: '日記を書く',
+        tabBarIcon: ({ tintColor }: { tintColor: string }): JSX.Element => (
+          <MaterialCommunityIcons name="pencil" size={25} color={tintColor} />
+        ),
+      },
+    },
+    YourDiary: {
       screen: YourDiaryListScreen,
       navigationOptions: {
         tabBarLabel: 'みんなの日記',
+        tabBarIcon: ({ tintColor }: { tintColor: string }): JSX.Element => (
+          <MaterialCommunityIcons
+            name="account-multiple"
+            size={25}
+            color={tintColor}
+          />
+        ),
       },
     },
   },
   {
     tabBarOptions: {
-      activeTintColor: 'blue',
-      inactiveTintColor: 'gray',
+      activeTintColor: mainColor,
     },
   }
 );
