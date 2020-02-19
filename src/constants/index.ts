971f652957d1e033139b1a613b00f5c8f1d19ac4
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2020-02-19 22:34:57 +0900

    ユーザプロフィール追加

---
+++ src/constants/index.ts
@@ -0,0 +1,15 @@
+import Constants from 'expo-constants';
+
+const isProduction = Constants.manifest.releaseChannel === 'production';
+
+let urlBase;
+if (isProduction) {
+  urlBase = 'https://interchao.com';
+} else if (__DEV__) {
+  urlBase = `https://staging.interchao.com/`;
+} else {
+  urlBase = 'http://maintenance.interchao.com';
+}
+
+export const URL_BASE = urlBase;
+export const SERVICE_NAME = 'Interchao';
