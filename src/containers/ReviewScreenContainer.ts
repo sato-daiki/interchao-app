c73c6d54499ef7b28d949f11f06419efe7bfec11
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2020-04-04 21:41:06 +0900

    レビュー画面の実装

---
+++ src/containers/ReviewScreenContainer.ts
@@ -0,0 +1,27 @@
+import { connect } from 'react-redux';
+import { NavigationStackProp } from 'react-navigation-stack';
+import { editDiary } from '../stores/actions/diaryList';
+import { State } from '../types/state';
+import ReviewScreen from '../screens/ReviewScreen';
+
+interface OwnProps {
+  navigation: NavigationStackProp;
+}
+
+const mapStateToProps = (state: State, ownProps: OwnProps) => {
+  const { diaries } = state.rootReducer.diaryList;
+  const { profile } = state.rootReducer;
+
+  const objectID = ownProps.navigation.getParam('objectID');
+  const diary = diaries.find(d => d.objectID === objectID);
+  return {
+    diary,
+    profile,
+  };
+};
+
+const mapDispatchToProps = {
+  editDiary,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewScreen);
