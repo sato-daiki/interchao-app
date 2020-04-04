76ec3e0e3d10f31817ecb456d7a20a46ea4ce3c4
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2020-04-04 21:41:54 +0900

    modalレビューは削除

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,5 +1,4 @@
 export { default as ModalAlertPublish } from './ModalAlertPublish';
-export { default as ModalReview } from './ModalReview';
 export { default as ModalLackPoint } from './ModalLackPoint';
 export { default as ModalAlertCorrection } from './ModalAlertCorrection';
 export { default as ModalBlock } from './ModalBlock';
