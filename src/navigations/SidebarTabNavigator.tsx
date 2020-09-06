e6627a1c9f6edf6b6a5594bec79bf6e303226c25
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2020-09-06 13:58:37 +0900

    sidebarの作成

--- src/navigations/SidebarTabNavigator.tsx
+++ src/navigations/SidebarTabNavigator.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { SafeAreaView, createNavigator, TabRouter } from 'react-navigation';
+import * as React from 'react';
+import { View, SafeAreaView, StyleSheet } from 'react-native';
+import {
+  NavigationHelpersContext,
+  TabRouter,
+  createNavigatorFactory,
+  useNavigationBuilder,
+  DefaultNavigatorOptions,
+  TabRouterOptions,
+  TabNavigationState,
+} from '@react-navigation/native';
+
+import { maxMain, borderLightColor } from '../styles/Common';
 import { SidebarTabs } from '../components/web/molecules';
-import { borderLightColor, maxMain } from '../styles/Common';
 
-// 有料サイトだがここ参考にした
-// https://medium.com/@bruno.eduardo.do/creating-a-custom-side-tab-navigator-with-react-navigation-28304b83e865
+// Supported screen options
+type TabNavigationOptions = {};
 
-// ソースはここ
-// https://github.com/bruno-edo/SidebarTabNavigator
+// Map of event name and the type of data (in event.data)
+//
+// canPreventDefault: true adds the defaultPrevented property to the
+// emitted events.
+type TabNavigationEventMap = {
+  tabPress: {
+    data: { isAlreadyFocused: boolean };
+    canPreventDefault: true;
+  };
+};
+
+// The props accepted by the component is a combination of 3 things
+type Props = DefaultNavigatorOptions<TabNavigationOptions> & TabRouterOptions;
+
 const styles = StyleSheet.create({
   safeAreaView: {
     flex: 1,
@@ -35,29 +56,41 @@ const styles = StyleSheet.create({
   },
 });
 
-const SidebarTabsNavigator = ({ navigation, descriptors }) => {
-  const { routes, index } = navigation.state;
-  const descriptor = descriptors[routes[index].key];
-
-  const ActiveScreen = descriptor.getComponent();
+const TabNavigator = ({
+  initialRouteName,
+  children,
+  screenOptions,
+}: Props): JSX.Element => {
+  const { state, navigation, descriptors } = useNavigationBuilder<
+    TabNavigationState,
+    TabRouterOptions,
+    TabNavigationOptions,
+    TabNavigationEventMap
+  >(TabRouter, {
+    children,
+    screenOptions,
+    initialRouteName,
+  });
 
   return (
-    <SafeAreaView style={styles.safeAreaView}>
-      <View style={styles.warapper}>
-        <View style={styles.container}>
-          <SidebarTabs navigation={navigation} />
-          <View style={styles.activeScreen}>
-            <ActiveScreen navigation={descriptor.navigation} />
+    <NavigationHelpersContext.Provider value={navigation}>
+      <SafeAreaView style={styles.safeAreaView}>
+        <View style={styles.warapper}>
+          <View style={styles.container}>
+            <SidebarTabs state={state} navigation={navigation} />
+            <View style={styles.activeScreen}>
+              {descriptors[state.routes[state.index].key].render()}
+            </View>
           </View>
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </NavigationHelpersContext.Provider>
   );
 };
 
-const createSidebarNavigator = (routeConfigMap, sidebarNavigatorConfig) => {
-  const customTabRouter = TabRouter(routeConfigMap, sidebarNavigatorConfig);
-  return createNavigator(SidebarTabsNavigator, customTabRouter, {});
-};
-
-export default createSidebarNavigator;
+export const createMyNavigator = createNavigatorFactory<
+  TabNavigationState,
+  TabNavigationOptions,
+  TabNavigationEventMap,
+  typeof TabNavigator
+>(TabNavigator);
