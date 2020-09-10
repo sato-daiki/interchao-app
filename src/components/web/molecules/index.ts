d3a9473f41848ee12c1152c0dee0bee344bbc29c
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2020-09-11 02:08:28 +0900

    deleted sidebar

--- src/components/web/molecules/index.ts
+++ src/components/web/molecules/index.ts
@@ -1,4 +1,3 @@
 export { default as AppDownload } from './AppDownload';
 export { default as Fotter } from './Fotter';
 export { default as Header } from './Header';
-export { default as SidebarTabs } from './SidebarTabs';
