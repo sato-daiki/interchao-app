6d83acc1144749cd6ba12f3c024cea6ae6642059
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2020-09-19 15:04:19 +0900

    add PublicNavigator

---
+++ src/navigations/PublicNavigator.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+// import UserDiaryScreenContainer from '../containers/UserDiaryScreenContainer';
+// import I18n from '../utils/I18n';
+
+// export type PublicStackParamList = {
+//   UserDiary: { objectID: string; userName: string };
+// };
+
+const Stack = createStackNavigator();
+
+const PublicNavigator = (): JSX.Element => {
+  return (
+    <Stack.Navigator initialRouteName="UserDiary">
+      {/* <Stack.Screen
+        name="UserDiary"
+        component={UserDiaryScreenContainer}
+        options={{ title: I18n.t('teachDiary.headerTitle') }}
+      /> */}
+    </Stack.Navigator>
+  );
+};
+
+export default PublicNavigator;
