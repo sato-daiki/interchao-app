9abd5a4811c99922cecc08b1519d1464cff14931
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2020-09-22 18:53:01 +0900

    音声登録途中まで

--- src/components/organisms/FairCopy.tsx
+++ src/components/organisms/FairCopy.tsx
@@ -1,10 +1,13 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Platform, ScrollView, StyleSheet, View } from 'react-native';
 import ViewShot from 'react-native-view-shot';
-import { ShareButton, Space } from '../atoms';
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
+import { GrayHeader, ShareButton, Space, WhiteButton } from '../atoms';
 import DiaryOriginal from './DiaryOriginal';
 
 import { Diary, Profile } from '../../types';
+import { mainColor, primaryColor } from '../../styles/Common';
+import ModalSpeech from './ModalSpeech';
 
 export interface Props {
   diary: Diary;
@@ -23,17 +26,45 @@ const styles = StyleSheet.create({
   scrollView: {
     flex: 1,
   },
-  shareButton: {
+  containerStyle: {
+    justifyContent: 'center',
+  },
+  button: {
     width: 300,
     alignSelf: 'center',
+    marginBottom: 16,
   },
 });
 
 const FairCopy: React.FC<Props> = ({ diary, profile }) => {
+  const [visibleSpeech, setVisibleSpeech] = useState(false);
   const viewShotRef = useRef<ViewShot | null>(null);
+
+  const onPressRecord = () => {};
+  const onPressMyVoiice = () => {};
+
+  const iconHeader = <AntDesign size={22} color={primaryColor} name="like1" />;
+
+  const iconMachine = (
+    <MaterialCommunityIcons size={22} color={mainColor} name="volume-high" />
+  );
+
+  const iconRecord = (
+    <MaterialCommunityIcons size={24} color={mainColor} name="microphone" />
+  );
+
+  const iconMyVoice = (
+    <MaterialCommunityIcons size={22} color={mainColor} name="headphones" />
+  );
 
   return (
     <View style={styles.container}>
+      <ModalSpeech
+        visible={visibleSpeech}
+        text={diary.fairCopyText || diary.text}
+        textLanguage={profile.learnLanguage}
+        onClose={(): void => setVisibleSpeech(false)}
+      />
       <ScrollView style={styles.scrollView}>
         <ViewShot
           style={styles.viewShot}
@@ -47,8 +78,33 @@ const FairCopy: React.FC<Props> = ({ diary, profile }) => {
             text={diary.fairCopyText || diary.text}
           />
         </ViewShot>
-        <Space size={48} />
-        <View style={styles.shareButton}>
+        <Space size={24} />
+        <GrayHeader
+          titleStyle={styles.containerStyle}
+          icon={iconHeader}
+          title="音読練習をする"
+        />
+        <Space size={24} />
+        <WhiteButton
+          containerStyle={styles.button}
+          icon={iconMachine}
+          title="機械の音声を聞く"
+          onPress={(): void => setVisibleSpeech(true)}
+        />
+        <WhiteButton
+          containerStyle={styles.button}
+          title="音声を録音する"
+          icon={iconRecord}
+          onPress={onPressRecord}
+        />
+        <WhiteButton
+          containerStyle={styles.button}
+          icon={iconMyVoice}
+          title="自分の音声を聞く"
+          onPress={onPressMyVoiice}
+        />
+        <Space size={32} />
+        <View style={styles.button}>
           {Platform.OS !== 'web' ? (
             <ShareButton
               viewShotRef={viewShotRef}
