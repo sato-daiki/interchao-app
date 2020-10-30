cb98aae4be0fb48f6dcd03c644b61c056bad61db
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2020-10-31 01:32:24 +0900

    fixed type error

--- src/containers/PostDiaryScreenContainer.ts
+++ src/containers/PostDiaryScreenContainer.ts
@@ -2,7 +2,6 @@ import { connect } from 'react-redux';
 import { State } from '@/types/state';
 import { setUser } from '@/stores/actions/user';
 import { addDiary } from '@/stores/actions/diaryList';
-import { setLocalStatus } from '@/stores/actions/localStatus';
 
 import PostDiaryScreen, {
   Props,
@@ -12,14 +11,12 @@ const mapStateToProps = (state: State): Props => {
   return {
     user: state.rootReducer.user,
     profile: state.rootReducer.profile,
-    localStatus: state.rootReducer.localStatus,
   };
 };
 
 const mapDispatchToProps = {
   setUser,
   addDiary,
-  setLocalStatus,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostDiaryScreen);
