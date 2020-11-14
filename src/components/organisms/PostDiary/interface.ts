0cd4a0d6b5682f9ee0368d6f45469ac45dc195d7
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2020-11-14 17:48:12 +0900

    add

--- src/components/organisms/PostDiary/interface.ts
+++ src/components/organisms/PostDiary/interface.ts
@@ -16,3 +16,35 @@ export interface PostDiaryKeyboardProps {
   onPressThemeGuide: () => void;
   fadeAnim?: Animated.Value;
 }
+
+export interface PostDiaryProps {
+  isLoading: boolean;
+  isModalLack: boolean;
+  isModalAlert: boolean;
+  isModalCancel: boolean;
+  isModalError: boolean;
+  isPublish: boolean;
+  isTutorialLoading?: boolean;
+  tutorialPostDiary?: boolean;
+  errorMessage: string;
+  title: string;
+  text: string;
+  themeCategory?: ThemeCategory | null;
+  themeSubcategory?: ThemeSubcategory | null;
+  publishMessage: string | null;
+  points: number;
+  learnLanguage: Language;
+  onPressSubmitModalLack: () => void;
+  onPressCloseModalLack: () => void;
+  onPressCloseModalPublish: () => void;
+  onPressCloseModalCancel: () => void;
+  onClosePostDiary: () => void;
+  onChangeTextTitle: (txt: string) => void;
+  onChangeTextText: (txt: string) => void;
+  onPressSubmit: () => void;
+  onPressDraft: () => void;
+  onPressNotSave: () => void;
+  onPressTutorial?: () => void;
+  onPressCloseError: () => void;
+  onPressThemeGuide: () => void;
+}
