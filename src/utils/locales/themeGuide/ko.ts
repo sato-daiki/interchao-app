8f70242f31bf77ab2db661902092ca10235bb5f5
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2020-11-25 15:59:25 +0900

    翻訳完了

--- src/utils/locales/themeGuide/ko.ts
+++ src/utils/locales/themeGuide/ko.ts
@@ -2,10 +2,11 @@ import { StyleType } from '@/components/organisms/ThemeGuide';
 
 export const koSelfIntroductionExpressions = [
   '안녕하세요.',
-  '처음 뵙겠습니다. 잘 부탁드립니다.',
-  '다나카 하나입니다. 하나라고 불러 주세요.',
-  '가나가와에서 태어나 도쿄에서 자랐습니다.',
-  '인터차오에서 마케팅 디렉터로 일하고 있습니다.',
+  '처음 뵙겠습니다.',
+  '제 이름은 __입니다. __라고 불러 주세요.',
+  '__에서 태어나 __에서 자랐습니다.',
+  '__에서 __(으)로 일하고 있습니다.',
+  '__(을)를 좋아합니다.',
   '감사합니다.',
 ];
 
@@ -14,301 +15,181 @@ export const koSelfIntroductionExamples = [
   [
     {
       key: 'a',
-      text: '처음 뵙겠습니다. 잘 부탁드립니다.',
+      text: '처음 뵙겠습니다.',
       styleType: StyleType.bold,
     },
   ],
   [
     { key: 'a', text: '제 이름은 ', styleType: StyleType.bold },
-    { key: 'b', text: '김철수', styleType: StyleType.p },
+    { key: 'b', text: '타나카하나', styleType: StyleType.p },
     { key: 'c', text: '입니다. ', styleType: StyleType.bold },
+    { key: 'd', text: '하나', styleType: StyleType.p },
+    { key: 'e', text: '라고 불러 주세요.', styleType: StyleType.bold },
   ],
   [
-    { key: 'a', text: '저는 ', styleType: StyleType.bold },
-    { key: 'b', text: '회사', styleType: StyleType.p },
-    { key: 'c', text: '에서  ', styleType: StyleType.bold },
-    { key: 'd', text: '무역', styleType: StyleType.p },
-    { key: 'e', text: '일을 하고 있습니다.', styleType: StyleType.bold },
+    { key: 'a', text: '가나가와', styleType: StyleType.p },
+    { key: 'b', text: '에서 태어나 ', styleType: StyleType.bold },
+    { key: 'c', text: '도쿄', styleType: StyleType.p },
+    { key: 'd', text: '에서 자랐습니다.', styleType: StyleType.bold },
   ],
   [
-    { key: 'a', text: '제 취미는 ', styleType: StyleType.bold },
-    { key: 'b', text: '사진을 찍는 것', styleType: StyleType.p },
-    { key: 'c', text: '입니다.', styleType: StyleType.bold },
+    { key: 'a', text: '인터차오', styleType: StyleType.p },
+    { key: 'b', text: '에서 ', styleType: StyleType.bold },
+    { key: 'c', text: '마케팅 디렉터', styleType: StyleType.p },
+    { key: 'd', text: '(으)로 일하고 있습니다.', styleType: StyleType.bold },
   ],
   [
-    { key: 'a', text: '저는 ', styleType: StyleType.bold },
-    { key: 'b', text: '서울 잠실', styleType: StyleType.p },
-    { key: 'c', text: '에 살고 있습니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '올해로 결혼한 지 ', styleType: StyleType.bold },
-    { key: 'b', text: '오', styleType: StyleType.p },
-    { key: 'c', text: '년째입니다.', styleType: StyleType.bold },
+    { key: 'a', text: '__', styleType: StyleType.p },
+    { key: 'b', text: '(을)를 좋아합니다. ', styleType: StyleType.bold },
   ],
   [{ key: 'a', text: '감사합니다.', styleType: StyleType.bold }],
 ];
 
 export const koHobbyExpressions = [
-  '제 취미는 달리기입니다.',
-  '저는 풀 마라톤에 참가했던 것을 계기로 달리기를 시작하게 되었습니다.',
-  '저는 앞으로도 운동을 계속해 나가고 싶습니다.',
+  '제 취미는 __입니다.',
+  '저는 _(을)를 계기로 _(을)를 시작하게 되었습니다.',
+  '저는 앞으로도 _(을)를 계속해 나가고 싶습니다.',
 ];
 
 export const koHobbyExamples = [
   [
-    { key: 'a', text: '제 취미는 ', styleType: StyleType.bold },
-    { key: 'b', text: '기타 연주', styleType: StyleType.p },
-    { key: 'c', text: '입니다.', styleType: StyleType.bold },
+    { key: 'a', text: '제 취미는 ', styleType: 'bold' },
+    { key: 'b', text: '달리기', styleType: 'p' },
+    { key: 'c', text: '입니다.', styleType: 'bold' },
   ],
   [
-    { key: 'a', text: '저는 ', styleType: StyleType.bold },
-    {
-      key: 'b',
-      text: '매일 기타를 2시간씩 연습합니다.',
-      styleType: StyleType.p,
-    },
-  ],
-  { key: 'a', text: '', styleType: '' },
-  [
-    {
-      key: 'a',
-      text: '제가 이 취미를 갖게 된 계기는 ',
-      styleType: StyleType.bold,
-    },
-    {
-      key: 'b',
-      text: '고등학교 시절 동아리 활동입니다.',
-      styleType: StyleType.p,
-    },
+    { key: 'a', text: '저는 ', styleType: 'bold' },
+    { key: 'b', text: '풀 마라톤에 참가했던 것', styleType: 'p' },
+    { key: 'c', text: '을 계기로 ', styleType: 'bold' },
+    { key: 'd', text: '달리기', styleType: 'p' },
+    { key: 'e', text: '를 시작하게 되었습니다.', styleType: 'bold' },
   ],
   [
-    { key: 'a', text: '올해로 ', styleType: StyleType.bold },
-    { key: 'b', text: '기타', styleType: StyleType.p },
-    { key: 'c', text: '를 시작한 지 ', styleType: StyleType.bold },
-    { key: 'd', text: '10', styleType: StyleType.p },
-    { key: 'e', text: '년째입니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '비용은 ', styleType: StyleType.bold },
-    {
-      key: 'b',
-      text: '기타만 구하면 많이 들지 않습니다.',
-      styleType: StyleType.p,
-    },
-  ],
-  [
-    { key: 'a', text: '앞으로 ', styleType: StyleType.bold },
-    {
-      key: 'b',
-      text: '더욱 열심히 해서 유튜브에 영상을 올리고 ',
-      styleType: StyleType.p,
-    },
-    { key: 'c', text: '싶습니다.', styleType: StyleType.bold },
+    { key: 'a', text: '저는 앞으로도 ', styleType: 'bold' },
+    { key: 'b', text: '운동', styleType: 'p' },
+    { key: 'c', text: '을 계속해 나가고 싶습니다.', styleType: 'bold' },
   ],
 ];
 
 export const koJobExpressions = [
-  '저는 Interchao Center에서 일하고 있습니다.',
-  '저의 직업은 영어 선생님입니다.',
-  '당사는 1:1 레슨을 전문으로 하고 있습니다.',
+  '저는 __에서 일하고 있습니다.',
+  '저의 직업은 __입니다.',
+  '당사는 __(을)를 전문으로 하고 있습니다.',
 ];
 
 export const koJobExamples = [
   [
-    { key: 'a', text: '저는 ', styleType: StyleType.bold },
-    { key: 'b', text: '교사', styleType: StyleType.p },
-    { key: 'c', text: '입니다.', styleType: StyleType.bold },
+    { key: 'a', text: '저는 ', styleType: 'bold' },
+    { key: 'b', text: 'Interchao Center', styleType: 'p' },
+    { key: 'c', text: '에서 일하고 있습니다.', styleType: 'bold' },
   ],
   [
-    { key: 'a', text: '저는 ', styleType: StyleType.bold },
-    { key: 'b', text: '중학교', styleType: StyleType.p },
-    { key: 'c', text: '에서 일하고 있습니다.', styleType: StyleType.bold },
+    { key: 'a', text: '저의 직업은 ', styleType: 'bold' },
+    { key: 'b', text: '영어 선생님', styleType: 'p' },
+    { key: 'c', text: '입니다.', styleType: 'bold' },
   ],
   [
-    { key: 'a', text: '하는 일은 ', styleType: StyleType.bold },
-    { key: 'b', text: '학생들을 가르치는 일', styleType: StyleType.p },
-    { key: 'c', text: '입니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '제 직업의 특징은 ', styleType: StyleType.bold },
-    { key: 'b', text: '출근시간이 이르다는 점입니다.', styleType: StyleType.p },
-  ],
-  [
-    {
-      key: 'a',
-      text: '제 직업에서 가장 마음에 드는 점은 ',
-      styleType: StyleType.bold,
-    },
-    { key: 'b', text: '방학이 있다는 점', styleType: StyleType.p },
-    { key: 'c', text: '입니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '제 직업에서 가장 힘든 점', styleType: StyleType.bold },
-    { key: 'b', text: '은 잦은 전근', styleType: StyleType.p },
-    { key: 'c', text: '입니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '교사', styleType: StyleType.p },
-    { key: 'b', text: '가 되려면 ', styleType: StyleType.bold },
-    { key: 'c', text: '사범대학 졸업증', styleType: StyleType.p },
-    { key: 'd', text: '이 필요합니다.', styleType: StyleType.bold },
+    { key: 'a', text: '당사는 ', styleType: 'bold' },
+    { key: 'b', text: '1:1 레슨', styleType: 'p' },
+    { key: 'c', text: '을 전문으로 하고 있습니다.', styleType: 'bold' },
   ],
 ];
 
-export const koStudyExpressions = [];
+export const koStudyExpressions = [
+  '제가 __를 공부하는 이유는 __ 때문입니다.',
+  '__(을)를 위해서는 __가 필수입니다.',
+  '__ 때문에 __ 시험 점수가 필요합니다.',
+];
 
 export const koStudyExamples = [
   [
-    { key: 'a', text: '직장', styleType: StyleType.p },
-    { key: 'b', text: '에서 ', styleType: StyleType.bold },
-    { key: 'c', text: '승진', styleType: StyleType.p },
-    { key: 'd', text: '하기 위해 ', styleType: StyleType.bold },
-    { key: 'e', text: '영어 ', styleType: StyleType.p },
-    { key: 'f', text: '실력이 필요합니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '영어', styleType: StyleType.p },
-    { key: 'b', text: '를 통해 ', styleType: StyleType.bold },
-    { key: 'c', text: '외국 친구를 사귀', styleType: StyleType.p },
-    { key: 'd', text: '고 싶습니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '영어', styleType: StyleType.p },
-    { key: 'b', text: '는 ', styleType: StyleType.bold },
-    { key: 'c', text: '국제화 시대의 필수 덕목', styleType: StyleType.p },
-    { key: 'd', text: '입니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '제가 ', styleType: StyleType.bold },
-    { key: 'b', text: '영어', styleType: StyleType.p },
-    { key: 'c', text: '를 공부하는 이유는 ', styleType: StyleType.bold },
-    { key: 'd', text: '외국인 남자친구 ', styleType: StyleType.p },
-    { key: 'e', text: '때문입니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '대학 수험', styleType: StyleType.p },
-    { key: 'b', text: '을 위해서는 ', styleType: StyleType.bold },
-    { key: 'c', text: '영어', styleType: StyleType.p },
-    { key: 'd', text: '가 필수입니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '대학원 진학 ', styleType: StyleType.p },
-    { key: 'b', text: '때문에 ', styleType: StyleType.bold },
-    { key: 'c', text: '영어 ', styleType: StyleType.p },
-    { key: 'd', text: '시험 점수가 필요합니다.', styleType: StyleType.bold },
-  ],
-];
-
-export const koDreamExpressions = [
-  '저의 꿈은 내 가게를 갖는 것입니다.',
-  '저는 어릴 적부터 요리를 좋아했습니다.',
-  '많은 사람이 제가 만든 음식을 먹고 기뻐했으면 좋겠습니다.',
-];
-
-export const koDreamExamples = [
-  [
-    { key: 'a', text: '제 미래의 꿈은 ', styleType: StyleType.bold },
-    { key: 'b', text: '만화가', styleType: StyleType.p },
-    { key: 'c', text: '입니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '저는 ', styleType: StyleType.bold },
-    { key: 'b', text: '그림 그리기', styleType: StyleType.p },
-    { key: 'c', text: '에 소질이 있습니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '예전부터 ', styleType: StyleType.bold },
-    { key: 'b', text: '악기 연주', styleType: StyleType.p },
-    { key: 'c', text: '를 좋아했습니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '사회에 보탬이 되는 ', styleType: StyleType.p },
-    { key: 'b', text: '사람이 되고 싶습니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '큰돈을 버는 ', styleType: StyleType.p },
-    { key: 'b', text: '것이 꿈입니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '따듯한 가정', styleType: StyleType.p },
-    { key: 'b', text: '을 갖고 싶습니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '아직 ', styleType: StyleType.bold },
     {
-      key: 'b',
-      text: '자신의 미래에 대해 생각해 본 적이 없습니다.',
+      key: 'a',
+      text: '제가 ',
+      styleType: StyleType.bold,
+    },
+    { key: 'b', text: '영어 ', styleType: StyleType.p },
+    { key: 'c', text: '를 공부하는 이유는 ', styleType: StyleType.bold },
+    {
+      key: 'd',
+      text: '새 직장에서 고객과 기본적으로 영어를 써야하기 때문입니다.',
       styleType: StyleType.p,
     },
   ],
 ];
 
+export const koDreamExpressions = [
+  '저의 꿈은 __입니다.',
+  '저는 어릴 적부터 __',
+  '__(으)면 좋겠습니다.',
+];
+
+export const koDreamExamples = [
+  [
+    { key: 'a', text: '저의 꿈은 ', styleType: 'bold' },
+    { key: 'b', text: '내 가게를 갖는 것', styleType: 'p' },
+    { key: 'c', text: '입니다.', styleType: 'bold' },
+  ],
+  [
+    { key: 'a', text: '저는 어릴 적부터 ', styleType: 'bold' },
+    { key: 'b', text: '요리를 좋아했습니다.', styleType: 'p' },
+  ],
+  [
+    {
+      key: 'a',
+      text: '많은 사람이 제가 만든 음식을 먹고 기뻐했',
+      styleType: 'p',
+    },
+    { key: 'b', text: '으면 좋겠습니다.', styleType: 'bold' },
+  ],
+];
+
 export const koTripExpressions = [
-  '저는 가족과 함께 이탈리아에 갔습니다.',
-  '우리는 10일간 로마와 베네치아, 나폴리를 방문했습니다.',
-  '가장 기억에 남는 건 보트를 타고 푸른 동굴에 갔던 것입니다.',
+  '저는 __(와)과 함께 _에 갔습니다.',
+  '우리는 __일간 __(을)를 방문했습니다.',
+  '가장 기억에 남는 건 __입니다.',
 ];
 
 export const koTripExamples = [
   [
-    { key: 'a', text: '저는 이제까지 ', styleType: StyleType.bold },
-    { key: 'b', text: '10개국', styleType: StyleType.p },
-    { key: 'c', text: '을 여행해 보았습니다.', styleType: StyleType.bold },
+    { key: 'a', text: '저는 ', styleType: 'bold' },
+    { key: 'b', text: '가족', styleType: 'p' },
+    { key: 'c', text: '과 함께 ', styleType: 'bold' },
+    { key: 'd', text: '이탈리아', styleType: 'p' },
+    { key: 'e', text: '에 갔습니다.', styleType: 'bold' },
   ],
   [
-    { key: 'a', text: '가장 좋았던 나라는 ', styleType: StyleType.bold },
-    { key: 'b', text: '프랑스', styleType: StyleType.p },
-    { key: 'c', text: '입니다.', styleType: StyleType.bold },
+    { key: 'a', text: '우리는 ', styleType: 'bold' },
+    { key: 'b', text: '10', styleType: 'p' },
+    { key: 'c', text: '일간 ', styleType: 'bold' },
+    { key: 'd', text: '로마와 베네치아, 나폴리', styleType: 'p' },
+    { key: 'e', text: '를 방문했습니다.', styleType: 'bold' },
   ],
   [
-    { key: 'a', text: '가장 ', styleType: StyleType.bold },
-    { key: 'b', text: '오래 ', styleType: StyleType.p },
-    { key: 'c', text: '여행한 기간은 ', styleType: StyleType.bold },
-    { key: 'd', text: '한 달 ', styleType: StyleType.p },
-    { key: 'e', text: '동안입니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '저는 ', styleType: StyleType.bold },
-    { key: 'b', text: '혼자서 ', styleType: StyleType.p },
-    { key: 'c', text: '여행하는 것을 즐깁니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '그곳은 ', styleType: StyleType.bold },
-    { key: 'b', text: '추웠지만, 인상에 남았습니다.', styleType: StyleType.p },
-  ],
-  [
-    { key: 'a', text: '오랫동안 기억에 남을 만한 ', styleType: StyleType.p },
-    { key: 'b', text: '여행이었습니다.', styleType: StyleType.bold },
+    { key: 'a', text: '가장 기억에 남는 건 ', styleType: 'bold' },
+    { key: 'b', text: '보트를 타고 푸른 동굴에 갔던 것', styleType: 'p' },
+    { key: 'c', text: '입니다.', styleType: 'bold' },
   ],
 ];
 
 export const koRebornExpressions = [
-  '다시 태어난다면 1945년쯤에 태어나고 싶습니다.',
-  '왜냐하면 1960년대 살았던 심리학자에게 배우고 싶기 때문입니다.',
+  '다시 태어난다면 __고 싶습니다.',
+  '왜냐하면 __(이)기 때문입니다.',
 ];
 
 export const koRebornExamples = [
   [
-    { key: 'a', text: '다시 태어난다면, 저는 ', styleType: StyleType.bold },
-    { key: 'b', text: '여자로 태어나고 싶습니다.', styleType: StyleType.p },
+    { key: 'a', text: '다시 태어난다면 ', styleType: 'bold' },
+    { key: 'b', text: '1945년쯤에 태어나', styleType: 'p' },
+    { key: 'c', text: '고 싶습니다.', styleType: 'bold' },
   ],
   [
-    { key: 'a', text: '왜냐하면, ', styleType: StyleType.bold },
-    { key: 'b', text: '군대에 들어가지 않아도 되기 ', styleType: StyleType.p },
-    { key: 'c', text: '때문입니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '가능하다면 ', styleType: StyleType.bold },
-    { key: 'b', text: '부자', styleType: StyleType.p },
-    { key: 'c', text: '로 태어나고 싶습니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '화목한 가정', styleType: StyleType.p },
-    { key: 'b', text: '에서 태어나고 싶습니다.', styleType: StyleType.bold },
-  ],
-  [
-    { key: 'a', text: '다시 태어나더라도, ', styleType: StyleType.bold },
-    { key: 'b', text: '저는 ', styleType: StyleType.p },
-    { key: 'c', text: '지금 인생을 살고 싶습니다.', styleType: StyleType.bold },
+    { key: 'a', text: '왜냐하면 ', styleType: 'bold' },
+    {
+      key: 'b',
+      text: '1960년대 살았던 심리학자에게 배우고 싶',
+      styleType: 'p',
+    },
+    { key: 'c', text: '기 때문입니다.', styleType: 'bold' },
   ],
 ];
