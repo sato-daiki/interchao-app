e536dad089cff2e1186437ad32e278d0b8ef76e7
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2022-02-20 16:20:42 +0900

    fix sentry

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,10 +1,10 @@
 import { Platform } from 'react-native';
 import * as Sentry from 'sentry-expo';
-// import * as Browser from '@sentry/browser';
+import * as Browser from '@sentry/browser';
 
 export const captureException = (message: any) => {
   if (Platform.OS === 'web') {
-    // Browser.captureException(message);
+    Browser.captureException(message);
   } else {
     Sentry.Native.captureException(message);
   }
