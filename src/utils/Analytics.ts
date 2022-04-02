20751851c76c3814392420376133997ed6dbaaaf
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2022-04-02 17:55:44 +0900

    fix

--- src/utils/Analytics.ts
+++ src/utils/Analytics.ts
@@ -1,7 +1,8 @@
-import * as Analytics from 'expo-firebase-analytics';
+// import * as Analytics from 'expo-firebase-analytics';
 
 export const logAnalytics = async (eventName: string) => {
-  await Analytics.logEvent(eventName);
+  console.log(eventName);
+  // await Analytics.logEvent(eventName);
 };
 
 export { default as events } from './events';
