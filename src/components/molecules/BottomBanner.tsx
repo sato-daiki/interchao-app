474b33373e67febca1778bcb1f81630baeac05a3
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2023-04-12 09:44:08 +0900

    admob動かす

--- src/components/molecules/BottomBanner.tsx
+++ src/components/molecules/BottomBanner.tsx
@@ -1,11 +1,6 @@
 import * as React from 'react';
-import { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, View, Platform } from 'react-native';
-// import {
-//   AdMobBanner,
-//   setTestDeviceIDAsync,
-//   requestPermissionsAsync,
-// } from 'expo-ads-admob';
+import { BannerAd, BannerAdSize } from 'react-native-google-mobile-ads';
 
 const styles = StyleSheet.create({
   adMobBanner: {
@@ -17,52 +12,21 @@ const IOS_AD_UNIT_ID = 'ca-app-pub-0770181536572634/3644229404';
 const ANDROID_AD_UNIT_ID = 'ca-app-pub-0770181536572634/3113100884';
 
 const BottomBanner: React.FC = () => {
-  const [loadingAdmobError, setLoadingAdmobError] = useState(false);
-  const [isPermission, setIsPermission] = useState(false);
-
-  useEffect(() => {
-    const f = async () => {
-      // if (Platform.OS === 'web') return;
-      // const { status } = await requestPermissionsAsync();
-      // console.log('status', status);
-      // if (status !== 'granted') {
-      //   setIsPermission(false);
-      // } else {
-      //   setIsPermission(true);
-      // }
-      // await setTestDeviceIDAsync('EMULATOR');
-    };
-    f();
-  }, []);
-
-  const onErrorLoadingAdMob = useCallback(() => {
-    setLoadingAdmobError(true);
-  }, []);
-
-  const renderAds = useCallback(() => {
-    if (Platform.OS === 'web') {
-      return null;
-    }
-
-    if (!loadingAdmobError) {
-      return (
-        <View style={styles.adMobBanner}>
-          {/* <AdMobBanner
-            bannerSize='smartBannerPortrait'
-            adUnitID={
-              Platform.OS === 'ios' ? IOS_AD_UNIT_ID : ANDROID_AD_UNIT_ID
-            }
-            servePersonalizedAds={isPermission}
-            onDidFailToReceiveAdWithError={onErrorLoadingAdMob}
-          /> */}
-        </View>
-      );
-    }
-
+  if (Platform.OS === 'web') {
     return null;
-  }, [isPermission, loadingAdmobError, onErrorLoadingAdMob]);
+  }
 
-  return <View>{renderAds()}</View>;
+  return (
+    <View style={styles.adMobBanner}>
+      {/* <BannerAd
+        unitId={Platform.OS === 'ios' ? IOS_AD_UNIT_ID : ANDROID_AD_UNIT_ID}
+        size={BannerAdSize.FULL_BANNER}
+        requestOptions={{
+          requestNonPersonalizedAdsOnly: true,
+        }}
+      /> */}
+    </View>
+  );
 };
 
 export default BottomBanner;
--- src/screens/MyDiaryListScreen/useFirstScreen.tsx
+++ src/screens/MyDiaryListScreen/useFirstScreen.tsx
@@ -1,6 +1,10 @@
-import { useEffect, useRef } from 'react';
-import { Subscription } from '@unimodules/core';
+import { useEffect, useRef, useCallback, useState } from 'react';
+import { AppState } from 'react-native';
+import mobileAds from 'react-native-google-mobile-ads';
+import { requestTrackingPermissionsAsync } from 'expo-tracking-transparency';
 import * as Notifications from 'expo-notifications';
+import { Subscription } from '@unimodules/core';
+
 import { LocalStatus } from '@/types';
 import { setPushotifications } from '@/utils/Notification';
 
@@ -18,17 +22,51 @@ export const useFirstScreen = ({
 }: Props): void => {
   const notificationListener = useRef<Subscription>();
   const responseListener = useRef<Subscription>();
+  const appState = useRef(AppState.currentState);
+  const [appStateVisible, setAppStateVisible] = useState(appState.current);
 
-  // 初期データの取得
+  const initPushotifications = useCallback(() => {
+    console.log('useFirstScreen', localStatus.firstLogin, localStatus.uid);
+    // 初回登録でskipした場合は入らないようにする
+    if (!localStatus.firstLogin && localStatus.uid) {
+      setPushotifications(localStatus.uid);
+    }
+  }, [localStatus.firstLogin, localStatus.uid]);
+
+  const initAd = useCallback(async () => {
+    const { status } = await requestTrackingPermissionsAsync();
+    if (status === 'granted') {
+      console.log('Yay! I have user permission to track data');
+    }
+    // await mobileAds().initialize();
+  }, []);
+
+  const initActiveFunctions = useCallback(() => {
+    initAd();
+  }, [initAd]);
+
+  const initFirstTimeOnlyFunctions = useCallback(() => {
+    initPushotifications();
+  }, [initPushotifications]);
+
   useEffect(() => {
-    const f = async (): Promise<void> => {
-      console.log('useFirstScreen', localStatus.firstLogin, localStatus.uid);
-      // 初回登録でskipした場合は入らないようにする
-      if (!localStatus.firstLogin && localStatus.uid) {
-        setPushotifications(localStatus.uid);
+    initFirstTimeOnlyFunctions();
+    const subscription = AppState.addEventListener('change', (nextAppState) => {
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === 'active'
+      ) {
+        // 画面がアクティブになったら呼ぶやつ
+        initActiveFunctions();
       }
+      appState.current = nextAppState;
+      setAppStateVisible(appState.current);
+      console.log('AppState', appState.current);
+    });
+
+    return () => {
+      subscription.remove();
     };
-    f();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -42,12 +80,11 @@ export const useFirstScreen = ({
     // );
 
     // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(
-      response => {
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
         console.log('[usePushNotification] catched notificationRes');
         onResponseReceived();
-      }
-    );
+      });
 
     return (): void => {
       // if (notificationListener.current) {
